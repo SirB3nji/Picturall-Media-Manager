5244080541d0333e189ca43921d53a5cab0e9a39
SirBenji <86887250+SirB3nji@users.noreply.github.com>
2022-11-21 16:43:25 +0100

    Add files via upload

--- scripts/pmm_graphicmode.py
+++ scripts/pmm_graphicmode.py
@@ -27,7 +27,7 @@ root.title('Picturall Media Manager.')
 
 root.config(background='#014976')
 
-root.iconbitmap('logo.ico')
+root.iconbitmap('misc/logo.ico')
 
 
 
@@ -43,9 +43,9 @@ def secondwindows():
     secondwindows.maxsize(width='800', height='400')
     secondwindows.title('Picturall Media Manager.')
     secondwindows.config(background='#014976')
-    secondwindows.iconbitmap('logo.ico')
+    secondwindows.iconbitmap('misc/logo.ico')
 
-    title_label = Label(secondwindows, text='Picturall Media Manager.', font=('Inter-Black.ttf', 20,'bold'), background='#014976', foreground='white')    
+    title_label = Label(secondwindows, text='Picturall Media Manager.', font=('misc/Inter-Black.ttf', 20,'bold'), background='#014976', foreground='white')    
     title_label.place(x=260, y=1)
 
     
@@ -60,7 +60,7 @@ def secondwindows():
 
         
         folder_media = "C:/Users/benjamin.cherchi/OneDrive - Analog Way SAS/Bureau/Projet_Stage/default_media/"
-        dest_ml = "C:/Users/benjamin.cherchi/OneDrive - Analog Way SAS/Bureau/Projet_Stage/misc/"
+        dest_ml = "misc/"
 
         ftp = FTP('192.168.2.161') 
         ftp.login(user='picmedia', passwd = 'aidemcip')
@@ -68,13 +68,13 @@ def secondwindows():
 
         print(ftp.getwelcome())
 
-        notification.notify(title = 'Picurall Media Manager', message = 'You are connected to the Picturall.', app_icon = 'logo.ico', timeout = 3)
+        notification.notify(title = 'Picurall Media Manager', message = 'You are connected to the Picturall.', app_icon = 'misc/logo.ico', timeout = 3)
 
         ftp.mkd('default_media')
         ftp.cwd('default_media')
 
 
-        notification.notify(title = 'Picurall Media Manager', message = 'Medias are being uploaded!', app_icon = 'logo.ico', timeout = 5)
+        notification.notify(title = 'Picurall Media Manager', message = 'Medias are being uploaded!', app_icon = 'misc/logo.ico', timeout = 5)
         for x in range(1, 65):
             clearoutput()
             print("Installation des medias en cours ... ")
@@ -133,13 +133,13 @@ def secondwindows():
         keyboard.write("exit")
         keyboard.press("enter")
 
-        notification.notify(title = 'Picurall Media Manager', message = 'Medias download is finished!', app_icon = 'logo.ico', timeout = 3)
+        notification.notify(title = 'Picurall Media Manager', message = 'Medias download is finished!', app_icon = 'misc/logo.ico', timeout = 3)
         messagebox.showinfo(title="PMM - INFO", message="Press 'OK' for reboot the Picturall.")
 
 
         
 
-        notification.notify(title = 'Picurall Media Manager', message = 'The picturall restart!', app_icon = 'logo.ico', timeout = 10)
+        notification.notify(title = 'Picurall Media Manager', message = 'The picturall restart!', app_icon = 'misc/logo.ico', timeout = 10)
 
 
 
@@ -150,9 +150,9 @@ def secondwindows():
         ftp.cwd('picturall/media')
 
         print(ftp.getwelcome())
-        notification.notify(title = 'Picurall Media Manager', message = 'You are connected to the Picturall.', app_icon = 'logo.ico', timeout = 0.5)
+        notification.notify(title = 'Picurall Media Manager', message = 'You are connected to the Picturall.', app_icon = 'misc/logo.ico', timeout = 0.5)
         time.sleep(1)
-        notification.notify(title = 'Picurall Media Manager', message = 'Medias are being deleted!', app_icon = 'logo.ico', timeout = 0.5)
+        notification.notify(title = 'Picurall Media Manager', message = 'Medias are being deleted!', app_icon = 'misc/logo.ico', timeout = 0.5)
 
         
 
@@ -197,7 +197,7 @@ def secondwindows():
 
 
         print("Suppression terminés !")
-        notification.notify(title = 'Picurall Media Manager', message = 'Deleting completed medias.', app_icon = 'logo.ico', timeout = 1)
+        notification.notify(title = 'Picurall Media Manager', message = 'Deleting completed medias.', app_icon = 'misc/logo.ico', timeout = 1)
 
 
     def function_playmdi_btn():
@@ -228,7 +228,7 @@ def secondwindows():
         keyboard.write("exit")
         keyboard.press("enter")
         print("the medias playback has started")
-        notification.notify(title = 'Picurall Media Manager', message = 'The medias playback has started.', app_icon = 'logo.ico', timeout = 2)
+        notification.notify(title = 'Picurall Media Manager', message = 'The medias playback has started.', app_icon = 'misc/logo.ico', timeout = 2)
 
     def function_stopmdi_btn():
         os.system("start cmd")
@@ -258,7 +258,7 @@ def secondwindows():
         keyboard.write("exit")
         keyboard.press("enter")
         print("the medias playback has stopped")
-        notification.notify(title = 'Picurall Media Manager', message = 'The medias playback has stopped.', app_icon = 'logo.ico', timeout = 2)
+        notification.notify(title = 'Picurall Media Manager', message = 'The medias playback has stopped.', app_icon = 'misc/logo.ico', timeout = 2)
     
 
     def function_pausemdi_btn():
@@ -289,7 +289,7 @@ def secondwindows():
         keyboard.write("exit")
         keyboard.press("enter")
         print("the medias playback has stopped")
-        notification.notify(title = 'Picurall Media Manager', message = 'The medias playback has paused.', app_icon = 'logo.ico', timeout = 2)
+        notification.notify(title = 'Picurall Media Manager', message = 'The medias playback has paused.', app_icon = 'misc/logo.ico', timeout = 2)
     
 
     def function_rebootpic_btn():
@@ -298,7 +298,7 @@ def secondwindows():
         client.connect('192.168.2.161', username='root', password='picturall')
         stdin, stdout, stderr = client.exec_command("reboot")
         client.close()
-        notification.notify(title = 'Picurall Media Manager', message = 'The Picturall reboot!', app_icon = 'logo.ico', timeout = 2)
+        notification.notify(title = 'Picurall Media Manager', message = 'The Picturall reboot!', app_icon = 'misc/logo.ico', timeout = 2)
 
 
 
@@ -327,58 +327,58 @@ def secondwindows():
 
 
 
-    dwnldmdi = PhotoImage(file='download.png')
+    dwnldmdi = PhotoImage(file='misc/download.png')
     dwnldmdi_btn = Button(secondwindows, image=dwnldmdi, border=0, bg='#014976', fg='#014976', activebackground='#014976', activeforeground='#014976', command=function_dwnlmdi_btn)
     dwnldmdi_btn.place(x=285, y=100)
 
-    download_label = Label(secondwindows, text="Install Media.", font=('Inter-Black.ttf', 20,'bold'), background='#014976', foreground='white')
+    download_label = Label(secondwindows, text="Install Media.", font=('misc/Inter-Black.ttf', 20,'bold'), background='#014976', foreground='white')
     download_label.place(x=340, y=110)
 
-    trashmdi = PhotoImage(file='trash.png')
+    trashmdi = PhotoImage(file='misc/trash.png')
     trashmdi_btn = Button(secondwindows, image=trashmdi, border=0, bg='#014976', fg='#014976', activebackground='#014976', activeforeground='#014976', command=function_trashmdi_btn)
     trashmdi_btn.place(x=283, y=197)
 
-    trash_label = Label(secondwindows, text="Delete Media.", font=('Inter-Black.ttf', 20,'bold'), background='#014976', foreground='white')
+    trash_label = Label(secondwindows, text="Delete Media.", font=('misc/Inter-Black.ttf', 20,'bold'), background='#014976', foreground='white')
     trash_label.place(x=340, y=210)
 
-    playmdi = PhotoImage(file="playmdi.png")
+    playmdi = PhotoImage(file="misc/playmdi.png")
     playmdi_btn = Button(secondwindows, image=playmdi, border=0, bg='#014976', fg='#014976', activebackground='#014976', activeforeground='#014976', command=function_playmdi_btn)
     playmdi_btn.place(x=10, y=340)
 
-    stopmdi = PhotoImage(file="stopmdi.png")
+    stopmdi = PhotoImage(file="misc/stopmdi.png")
     stopmdi_btn = Button(secondwindows, image=stopmdi, border=0, bg='#014976', fg='#014976', activebackground='#014976', activeforeground='#014976', command=function_stopmdi_btn)
     stopmdi_btn.place(x=70, y=340)
 
-    pausemdi = PhotoImage(file="pausemdi.png")
+    pausemdi = PhotoImage(file="misc/pausemdi.png")
     pausemdi_btn = Button(secondwindows, image=pausemdi, border=0, bg='#014976', fg='#014976', activebackground='#014976', activeforeground='#014976', command=function_pausemdi_btn)
     pausemdi_btn.place(x=130, y=340)
 
-    rebootpic = PhotoImage(file="reboot.png")
+    rebootpic = PhotoImage(file="misc/reboot.png")
     rebootpic_btn = Button(secondwindows, image=rebootpic, border=0, bg='#014976', fg='#014976', activebackground='#014976', activeforeground='#014976', command=function_rebootpic_btn)
     rebootpic_btn.place(x=743, y=340)
 
 
     secondwindows.mainloop()
 
-title_label = Label(root, text='Picturall Media Manager.', font=('Inter-Black.ttf', 20,'bold'), background='#014976', foreground='white')    
+title_label = Label(root, text='Picturall Media Manager.', font=('misc/Inter-Black.ttf', 20,'bold'), background='#014976', foreground='white')    
 title_label.place(x=260, y=1)
 
 
-login_label = Label(root, text='login.', font=('Inter-Black.ttf', 16,'bold'), background='#014976', foreground='white')
+login_label = Label(root, text='login.', font=('misc/Inter-Black.ttf', 16,'bold'), background='#014976', foreground='white')
 login_label.place(x=380, y=155)
 
 
 cmd_login_btn = secondwindows
-login_image_btn = PhotoImage(file='login2.png')
+login_image_btn = PhotoImage(file='misc/login2.png')
 login_btn = Button(root, image=login_image_btn, border=0, bg='#014976', fg='#014976', activebackground='#014976', activeforeground='#014976', command=cmd_login_btn)
 login_btn.place(x=380, y=90)
 
 cmd_quit_btn = root.quit
-quit_image_btn = PhotoImage(file='quit2.png')
+quit_image_btn = PhotoImage(file='misc/quit2.png')
 quit_btn = Button(root, image=quit_image_btn, border=0, bg='#014976', fg='#014976', activebackground='#014976', activeforeground='#014976', command=cmd_quit_btn)
 quit_btn.place(x=378, y=220)
 
-quit_label = Label(root, text='quit.', font=('Inter-Black.ttf', 16,'bold'), background='#014976', foreground='white')
+quit_label = Label(root, text='quit.', font=('misc/Inter-Black.ttf', 16,'bold'), background='#014976', foreground='white')
 quit_label.place(x=382, y=280)
 
 mainloop()
